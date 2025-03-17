cc59be1689d38d0245610bb8710a432485eb9a75
LoanTran03 <dumpi2003@gmail.com>
2025-03-17 15:21:44 +0700

    mongoose, database config

---
+++ config/database.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const mongoose = require('mongoose');
+
+module.exports.connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Connect to database successfully");
+    } catch (error) {
+        console.log("Connect to database failed");
+    }
+}
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const route = require("./routes/client/index.route");
+const database = require("./config/database");
 
 const app = express();
 const port = process.env.PORT || 3000; // Thêm giá trị mặc định nếu PORT không được định nghĩa
+
+database.connect();
+mongoose.connect(process.env.MONGO_URL);
 
 app.set("view engine", "pug");
 app.set("views", "./views"); // Đảm bảo thư mục 'views' tồn tại
