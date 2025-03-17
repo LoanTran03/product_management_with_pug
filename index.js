6e683242e6f0b344c8c859388408d7949122ae66
LoanTran03 <dumpi2003@gmail.com>
2025-03-17 14:49:31 +0700

    dung khung

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const express = require("express");
 const route = require("./routes/client/index.route");
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000; // Thêm giá trị mặc định nếu PORT không được định nghĩa
 
 app.set("view engine", "pug");
 app.set("views", "./views"); // Đảm bảo thư mục 'views' tồn tại
 
-app.use(express.static("public")); // Đảm bảo thư mục 'public' tồn tại
+app.use(express.json());
+app.use(express.static("public")); // Phục vụ file tĩnh từ thư mục public
 
 route(app);
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
-    }
-);
+});
